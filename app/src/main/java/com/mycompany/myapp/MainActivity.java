647fd4846144a907c63e6ae1246bf5fa4cce1c04
mgzimu <1805668432@qq.com>
2016-12-01 15:20:23 +0800

    退出增加吐司 退出

--- app/src/main/java/com/mycompany/myapp/MainActivity.java
+++ app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.mycompany.myapp;
 
 import android.app.*;
 import android.os.*;
+import android.widget.*;
 
 public class MainActivity extends Activity 
 {
@@ -11,4 +12,11 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+	@Override
+	protected void onDestroy()
+	{
+		Toast.makeText(MainActivity.this,"退出",Toast.LENGTH_SHORT).show();
+	}
+	
 }
